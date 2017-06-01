06893e8507f8e40c500a489d82db48a2890c49b3
牛加人等于朱 <niuren.zhu@icloud.com>
2017-06-01 17:07:36 +0800

    调整脚本

--- ibas.initialfantasy.service/src/main/webapp/index.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", "./bsapp/Console"], function (require, exports, Console_1) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.default = Console_1.Console;
-});
