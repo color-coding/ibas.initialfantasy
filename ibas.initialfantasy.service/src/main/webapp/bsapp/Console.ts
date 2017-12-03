563800f9748a0a7c81f702fe20b3f770105c54c8
niurenzhu <niuren.zhu@icloud.com>
2017-12-03 12:36:27 +0800

    独立枚举资源文件

--- ibas.initialfantasy.service/src/main/webapp/bsapp/Console.ts
+++ ibas.initialfantasy.service/src/main/webapp/bsapp/Console.ts
@@ -77,6 +77,7 @@ export class Console extends ibas.ModuleConsole {
     run(): void {
         // 加载语言-框架默认
         ibas.i18n.load(this.rootUrl + "resources/languages/initialfantasy.json");
+        ibas.i18n.load(this.rootUrl + "resources/languages/enums.json");
         ibas.i18n.load(this.rootUrl + "resources/languages/bo/applicationfunction.json");
         ibas.i18n.load(this.rootUrl + "resources/languages/bo/applicationmodule.json");
         ibas.i18n.load(this.rootUrl + "resources/languages/bo/applicationplatform.json");
