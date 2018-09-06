e50093e4d4e101c278f619f06e52e852ff4011d6
牛加人等于朱 <niuren.zhu@icloud.com>
2018-09-06 15:55:04 +0800

    优化交互

--- ibas.initialfantasy.service/src/main/webapp/bsui/c/applicationelement/ApplicationElementChooseView.ts
+++ ibas.initialfantasy.service/src/main/webapp/bsui/c/applicationelement/ApplicationElementChooseView.ts
@@ -95,6 +95,7 @@ namespace initialfantasy {
                         buttons: [
                             new sap.m.Button("", {
                                 text: ibas.i18n.prop("shell_data_new"),
+                                visible: this.mode === ibas.emViewMode.VIEW ? false : true,
                                 type: sap.m.ButtonType.Transparent,
                                 press: function (): void {
                                     that.fireViewEvents(that.newDataEvent);
--- ibas.initialfantasy.service/src/main/webapp/bsui/c/user/UserChooseView.ts
+++ ibas.initialfantasy.service/src/main/webapp/bsui/c/user/UserChooseView.ts
@@ -100,6 +100,7 @@ namespace initialfantasy {
                         buttons: [
                             new sap.m.Button("", {
                                 text: ibas.i18n.prop("shell_data_new"),
+                                visible: this.mode === ibas.emViewMode.VIEW ? false : true,
                                 type: sap.m.ButtonType.Transparent,
                                 // icon: "sap-icon://create",
                                 press: function (): void {
